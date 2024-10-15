12e3214aafca021334ca68ca64a943b2fff28208
Tejas <Tejas@DESKTOP-SS35PNP>
2024-10-15 22:11:06 +0530

    Add project files.

---
+++ insertstudentinMvc/Controllers/StudentController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using insertstudentinMvc.Models;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace insertstudentinMvc.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public String Index(StudentReg std)
+        {
+
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-SS35PNP\\SQLEXPRESS;Initial Catalog=DBstudent;Integrated Security=True;Encrypt=True;TrustServerCertificate=True");
+            SqlDataAdapter adp = new SqlDataAdapter("Select * from tblstudent",con);
+            SqlCommandBuilder cbd= new SqlCommandBuilder(adp);
+
+            DataSet ds = new DataSet();
+            adp.Fill(ds, "tblstudent");
+
+            DataRow dr = ds.Tables["tblstudent"].NewRow();
+
+            dr["Id"]=std.Id;
+            dr["Name"]=std.Name;
+            dr["Class"]=std.Class;
+
+            ds.Tables["tblstudent"].Rows.Add(dr);
+
+            int n = adp.Update(ds,"tblstudent");
+            if(n> 0 )
+            {
+                return "record inserted";
+            }
+            else
+            {
+                return "record not inserted";
+            }
+            
+        }
+    }
+}
---
+++ insertstudentinMvc/Models/StudentRef.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace insertstudentinMvc.Models
+{
+    public class StudentReg
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Class { get; set; }
+    }
+    }
